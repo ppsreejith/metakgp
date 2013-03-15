3e3560801ba2f22b8b195880166057e58f763a28
Vikrant Varma <vikrant.varma94@gmail.com>
2013-03-15 22:06:48 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metakgp::Application.config.secret_token = '1a886d01f348abd4b010c3f7f3dbfd8ba300c50888a8f53544c02b243450af993b24101eb44f66ca4fe13819c800ed3066fcd2425f6643eed242835d3cc7abc2'
